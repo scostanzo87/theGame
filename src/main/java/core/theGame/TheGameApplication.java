da780273d6abbb85db0b2f1234755d395d433b75
scostanzo87 <stefano.costanzo87@gmail.com>
2024-05-05 12:45:14 +0200

    initial spring boot 3 commit

---
+++ src/main/java/core/theGame/TheGameApplication.java
@@ -0,0 +1,13 @@
+package core.theGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/theGame/TheGameApplicationTests.java
@@ -0,0 +1,13 @@
+package core.theGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
